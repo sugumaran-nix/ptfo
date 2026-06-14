0cf2347ef806572448e0f81e2f7af3ff44e0e225
Sugumaran <141924234+sugumaran-nix@users.noreply.github.com>
2026-06-14 13:32:06 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  output: "export",
-  images: { unoptimized: true },
-};
-module.exports = nextConfig;
